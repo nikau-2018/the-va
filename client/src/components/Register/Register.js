922d3608fb68f775ff6fefb4992878ffbdb77741
Lisa <zuzatoy@gmail.com>
2018-09-20 13:40:43 +1200

    creat Actions and Reducers

---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,9 @@
+
+  import React from 'react'
+  import { Link } from 'react-router-dom';
+  import { Button } from 'semantic-ui-react';
+
+  export class Register extends React.Component {
+
+    
+  }
