b7e167eb834a3ee1dd7e7868fabb1f723697270e
bryceknz <bryceknz@gmail.com>
2018-09-20 13:21:24 +1200

    Changed 'username' to 'display_name'

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Post = ({post}) => (
   <div>
     <h4>{post.title}</h4>
-    <h6>{post.username}</h6>
+    <h6>{post.display_name}</h6>
     <p>{Date(post.date)}</p>
     <p>{post.body}</p>
   </div>
