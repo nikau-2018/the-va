07707c1cf7c7df26f403df96dc647e26188dc10a
georgehardy <george.hardy@gmail.com>
2018-09-20 15:36:34 +1200

    /api/v1/posts:id GET route implemented. Fixes #11

--- server/db/posts.js
+++ server/db/posts.js
@@ -1,10 +1,39 @@
 const connection = require('./')
 
 module.exports = {
-  getPosts
+  getPosts,
+  getPostWithReplies
 }
 
 // Sample only: think about what you want this function to actually do...
 function getPosts (db = connection) {
   return db('posts').select()
+}
+
+function getPostWithReplies (postId, db = connection) {
+  const postData = {
+    post: {},
+    replies: []
+  }
+  return getPost(postId, db)
+    .then(post => {
+      postData.post = post
+      return getReplies(postId, db)
+        .then(replies => {
+          postData.replies = replies
+          return postData
+        })
+    })
+}
+
+function getPost (postId, db) {
+  return db('posts')
+    .select()
+    .where('id', postId)
+}
+
+function getReplies (postId, db) {
+  return db('replies')
+    .select()
+    .where('postId', postId)
 }
