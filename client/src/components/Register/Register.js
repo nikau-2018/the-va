971748362cbd05a24baf7849be5ede3b67ab4b04
bryceknz <bryceknz@gmail.com>
2018-09-20 14:37:39 +1200

    Update Register.js

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,9 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
-  import React from 'react'
-  import { Link } from 'react-router-dom';
-  import { Button } from 'semantic-ui-react';
+export class Register extends React.Component {
 
-  export class Register extends React.Component {
 
-    
-  }
+}
