a5d0c7f71abd3c3329d04e2b9d023262ddcfb69d
bryceknz <bryceknz@gmail.com>
2018-09-21 13:54:51 +1200

    Update WelcomePage.js

--- client/src/components/Welcome/WelcomePage.js
+++ client/src/components/Welcome/WelcomePage.js
@@ -3,20 +3,18 @@ import { Link } from 'react-router-dom'
 
 export default function WelcomePage (props) {
   return (
-    <div className='container welcomePage'>
-      <div className='welcomeToTheVa'>
-        <h1>
-          WELCOME <br />
-          TO <br />
-          <span>The Vā</span>
-        </h1>
-        <Link to="/login">
-          <button>Login</button>
-        </Link><br />
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
-      </div>
+    <div className='welcomeToTheVa'>
+      <h1>
+        WELCOME <br />
+        TO <br />
+        <span>The Vā</span>
+      </h1>
+      <Link to="/login">
+        <button>Login</button>
+      </Link><br />
+      <Link to="/register">
+        <button>Register</button>
+      </Link>
     </div>
   )
 }
