07c14cfffd97baf355fe6901e58b28025566724f
georgehardy <george.hardy@gmail.com>
2018-09-21 16:29:12 +1200

    fixip

--- client/src/components/CreatePostMain.jsx
+++ client/src/components/CreatePostMain.jsx
@@ -34,7 +34,7 @@ class CreatePost extends React.Component {
       }
     }
 
-    axios.post('http://192.168.1.183:3001/api/v1/posts/', postData)
+    axios.post('http://localhost:3001/api/v1/posts/', postData)
       .then(response => {
         this.setState({done: true})
       })
