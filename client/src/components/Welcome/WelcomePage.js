5fe93b28f6233bdd4e6d9bec330e47a983ab0d98
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-21 16:12:41 +1200

    Working on CSS

--- client/src/components/Welcome/WelcomePage.js
+++ client/src/components/Welcome/WelcomePage.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function WelcomePage (props) {
   return (
-    <div className='welcomeToTheVa'>
-      <h1>
+    <div className='app'>
+      <div className='border'/>
+      <div className='welcomeToTheVa'>
+        <h1>
         WELCOME <br />
         TO <br />
-        <span>The V<span className='macron'>a</span></span>
-      </h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link><br />
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+          <span>The V<span className='macron'>a</span></span>
+        </h1>
+        <Link to="/login">
+          <button>Login</button>
+        </Link><br />
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
     </div>
   )
 }
