99e38b164e49b3eb9b99cd5ab9e80d6abfc31089
georgehardy <george.hardy@gmail.com>
2018-09-24 11:50:56 +1200

    Add replies to redux store. Set initial state for posts to be an object. Fixes #83

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import errorMessage from './error-message'
 import posts from './posts'
+import replies from './replies'
 import waiting from './waiting'
 
 export default combineReducers({
   errorMessage,
   posts,
+  replies,
   waiting
 })
