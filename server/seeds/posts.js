611c97f8622f3b6b4c174708c9a5ee3fdbbf23ef
bryceknz <bryceknz@gmail.com>
2018-09-24 09:56:33 +1200

    Update posts.js

--- server/seeds/posts.js
+++ server/seeds/posts.js
@@ -2,7 +2,6 @@ exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('posts').del()
     .then(function () {
-      // Inserts seed entries
       return knex('posts').insert([
         {id: 2000, user_id: 1000, display_name: 'pillow123', title: 'Church girl?', body: 'My whole family is churchy, but I just don’t think I have the same beliefs as them. I don’t know what to do', upvotes: 0, downvotes: 0, reported: false, created_at: Date.now()},
         {id: 2001, user_id: 1001, display_name: 'Tulip.cake', title: 'Over it', body: 'tired of being micromanaged. I should be able to do what I want and wear what I want without worrying about being judged by my family ', upvotes: 0, downvotes: 0, reported: false, created_at: Date.now()},
