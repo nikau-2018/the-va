37fa67b4021f4da0c20295bfc4e4eac68e7811a7
georgehardy <george.hardy@gmail.com>
2018-09-24 11:51:36 +1200

    remove unused import

--- client/src/components/PostDetail.jsx
+++ client/src/components/PostDetail.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import request from 'axios'
 import {fetchPosts, fetchReplies} from '../actions'
 
 class PostDetail extends React.Component {
