16503edc05441e5f0ec80ec3bd39bbbff9e88484
georgehardy <george.hardy@gmail.com>
2018-09-25 11:22:13 +1200

    set initial displayName back to blank

--- client/src/components/PostDetail.jsx
+++ client/src/components/PostDetail.jsx
@@ -9,7 +9,7 @@ class PostDetail extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      displayName: 'f',
+      displayName: '',
       replyText: ''
     }
   }
