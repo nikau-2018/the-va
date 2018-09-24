bacf3868106ee9fce37951142599bfab78d392fd
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-24 12:33:31 +1200

    Added login-greeting to css and working on incorporating component into Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,27 +1,43 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function Home (props) {
-  return (
-    <div className="home">
-      <h1>Rules</h1>
-      <p>Please read through the following rules and abide by them to ensure a safe community:</p>
-      <ul>
-        <li>Be patient</li>
-        <li>Be kind</li>
-        <li>Be respectful</li>
-      </ul>
-      <div className="bottom-tabs">
-        <Link to="/myposts">
-          <button>Me</button>
-        </Link>
-        <Link to="/list">
-          <button>Talanoa</button>
-        </Link>
-        <Link to="/village">
-          <button>Village</button>
-        </Link>
+import LoginGreeting from '../LoginGreeting'
+
+class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      justLoggedIn: false
+    }
+  }
+
+  // onClick
+
+  render () {
+    return (
+      <div className="home">
+        <h1>Rules</h1>
+        <p>Please read through the following rules and abide by them to ensure a safe community:</p>
+        <ul>
+          <li>Be patient</li>
+          <li>Be kind</li>
+          <li>Be respectful</li>
+        </ul>
+        <LoginGreeting />
+        <div className="bottom-tabs">
+          <Link to="/myposts">
+            <button>Me</button>
+          </Link>
+          <Link to="/list">
+            <button>Talanoa</button>
+          </Link>
+          <Link to="/village">
+            <button>Village</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default Home
