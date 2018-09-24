32134c064ac31b345f814c241159d2b3d5c8f1ce
Zaine Kingi <rous6026nz@gmail.com>
2018-09-24 18:01:25 +1200

    Implement fixes for register user and login user.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,11 +5,13 @@ import posts from './posts'
 import replies from './replies'
 import waiting from './waiting'
 import justLoggedIn from './justLoggedIn'
+import loginReducers from './login'
 
 export default combineReducers({
   errorMessage,
   posts,
   replies,
   waiting,
-  justLoggedIn
+  justLoggedIn,
+  loginReducers
 })
