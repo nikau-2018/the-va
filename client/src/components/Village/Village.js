b1d8af21895786b4c3d16fb086907827cdc0ba8c
emma-nio <emmanioashley@gmail.com>
2018-09-25 14:09:12 +1200

    added React player dependency

--- client/src/components/Village/Village.js
+++ client/src/components/Village/Village.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import ReactSoundcloud from 'react-soundcloud'
+import ReactPlayer from 'react-player'
 
 const Village = (props) => {
   return (
     <div className='village'>
       <h2><span className='underline underlineTriangles'>Talanoa</span></h2>
-      React.render(<ReactSoundcloud url="https://soundcloud.com/jahrarager/a-first-reading-i-say-my-name-with-a-mouth-full-of-dirt"/>
+      <div>
+        <ReactPlayer className='player' width="60%" height="30%" url='https://soundcloud.com/jahrarager/a-first-reading-i-say-my-name-with-a-mouth-full-of-dirt' />
+      </div>
     </div>
   )
 }
