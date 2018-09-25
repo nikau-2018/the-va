cd582f4a699e43538e3b4d884a6a75d3bb4634c6
georgehardy <george.hardy@gmail.com>
2018-09-25 23:03:35 +1200

    remove created_at and let the database do automatically

--- server/seeds/c_replies.js
+++ server/seeds/c_replies.js
@@ -4,9 +4,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('replies').insert([
-        {id: 3001, post_id: 2000, user_id: 1003, display_name: 'replier_disp', text: 'church reply', upvotes: 0, downvotes: 0, reported: false, created_at: Date.now()},
-        {id: 3002, post_id: 2002, user_id: 1003, display_name: 'replier_disp', text: 'over it reply', upvotes: 0, downvotes: 0, reported: false, created_at: Date.now()},
-        {id: 3003, post_id: 2002, user_id: 1003, display_name: 'replier_disp', text: 'ngatu reply', upvotes: 0, downvotes: 0, reported: false, created_at: Date.now()}
+        {id: 3001, post_id: 2000, user_id: 1003, display_name: 'replier_disp', text: 'church reply', upvotes: 0, downvotes: 0, reported: false},
+        {id: 3002, post_id: 2002, user_id: 1003, display_name: 'replier_disp', text: 'over it reply', upvotes: 0, downvotes: 0, reported: false},
+        {id: 3003, post_id: 2002, user_id: 1003, display_name: 'replier_disp', text: 'ngatu reply', upvotes: 0, downvotes: 0, reported: false}
       ])
     })
 }
