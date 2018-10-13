27603560f7f191acdef5d07da07cd8eb7209aca7
georgehardy <george.hardy@gmail.com>
2018-10-13 17:00:11 +1300

    fix indentation and dodgy import

--- client/src/actions/register.js
+++ client/src/actions/register.js
@@ -1,42 +1,40 @@
 import request from 'axios'
-import { dispatch } from '../../../node_modules/rxjs/internal/observable/pairs';
 
 export const REGISTER_REQUEST = 'REGISTER_REQUEST'
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 export const REGISTER_ERROR = 'REGISTER_ERROR'
 
-
 export const requestRegister = () => {
-    return {
-        type: REGISTER_REQUEST
-    }
+  return {
+    type: REGISTER_REQUEST
+  }
 }
 
 export const registerSuccess = (user) => {
-    console.log(user)
-    return {
-        type: REGISTER_SUCCESS,
-        user: user 
-    }
+  console.log(user)
+  return {
+    type: REGISTER_SUCCESS,
+    user: user
+  }
 }
 
 export const registerError = (errMes) => {
-    return {
-        type: REGISTER_ERROR,
-        errMes: errMes
-    }
+  return {
+    type: REGISTER_ERROR,
+    errMes: errMes
+  }
 }
 
 export function fetchRegister () {
-    return (dispatch) => {
-        dispatch(requestRegister())
-        return request
-            .get('/api/users/register')
-            .then (res => {
-                dispatch(registerSuccess(res.data.users))
-            })
-            .catch(err => {
-                dispatch(registerError(err.message))
-            })
-    }
+  return (dispatch) => {
+    dispatch(requestRegister())
+    return request
+      .get('/api/users/register')
+      .then(res => {
+        dispatch(registerSuccess(res.data.users))
+      })
+      .catch(err => {
+        dispatch(registerError(err.message))
+      })
+  }
 }
